fdd3644e58ca4842dc28ae65fa2726c1099f4d2f
Webfael <rafaelfaustrodrigues@gmail.com>
2023-05-02 16:35:44 -0300

    iframe

--- src/components/CarouselItens/index.js
+++ src/components/CarouselItens/index.js
@@ -221,7 +221,7 @@ function CarouselItens() {
           <img src={onix} alt="Imagem de segurança" />
         </DivImage>
         <DivText>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSesxlkTz9rjZ3hSQCIimiAzvD6_eNK8HvSe8f0p34F8XGSybQ/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSesxlkTz9rjZ3hSQCIimiAzvD6_eNK8HvSe8f0p34F8XGSybQ/viewform?embedded=true" width="90%" height="90%" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
         
         </DivText>
       </Item>
